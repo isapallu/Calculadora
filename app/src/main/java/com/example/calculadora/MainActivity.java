a3a05213c3196ba5edd41a4396982f825b5b8366
isadora_p_correa <isadora_p_correa@estudante.sesisenai.org.br>
2023-03-22 09:52:47 -0300

    metodo soma

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText n1;
+    EditText n2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        n1 = findViewById(R.id.n1);
+        n2 = findViewById(R.id.n2);
+
     }
     public void Soma(View v){
+        double v1 = Double.parseDouble(n1.getText().toString());
+        double v2 = Double.parseDouble(n2.getText().toString());
 
     }
 
