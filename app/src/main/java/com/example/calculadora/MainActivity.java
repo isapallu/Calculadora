2cdf0d7bdbea3d30e025946a5265973571f8b293
isadora_p_correa <isadora_p_correa@estudante.sesisenai.org.br>
2023-03-23 12:23:18 -0300

    teste

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -23,17 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void Soma(View v){
-        vazio();
-
         double v1 = Double.parseDouble(n1.getText().toString());
         double v2 = Double.parseDouble(n2.getText().toString());
-
+        vazioS();
         double sm = v1 + v2;
         resultadoConta.setText(sm + "");
     }
     public void Subtracao(View v){
-        vazio();
-
         double x1 = Double.parseDouble(n1.getText().toString());
         double x2 = Double.parseDouble(n2.getText().toString());
 
@@ -41,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
         resultadoConta.setText(sub + "");
     }
     public void Divisao(View v){
-        vazio();
-
         double f1 = Double.parseDouble(n1.getText().toString());
         double f2 = Double.parseDouble(n2.getText().toString());
 
@@ -50,33 +44,20 @@ public class MainActivity extends AppCompatActivity {
         resultadoConta.setText(div + "");
     }
     public void Multiplicacao(View v){
-        vazio();
         double m1 = Double.parseDouble(n1.getText().toString());
         double m2 = Double.parseDouble(n2.getText().toString());
 //
         double mult = m1*m2;
         resultadoConta.setText(mult + "");
     }
-    public void vazio(){
+    public void vazioS(){
         try{
             double v1 = Double.parseDouble(n1.getText().toString());
             double v2 = Double.parseDouble(n2.getText().toString());
-            double x1 = Double.parseDouble(n1.getText().toString());
-            double x2 = Double.parseDouble(n2.getText().toString());
-            double f1 = Double.parseDouble(n1.getText().toString());
-            double f2 = Double.parseDouble(n2.getText().toString());
-            double m1 = Double.parseDouble(n1.getText().toString());
-            double m2 = Double.parseDouble(n2.getText().toString());
         }
         catch(Exception e){
             double v1 = 0;
             double v2 = 0;
-            double x1 = 0;
-            double x2 = 0;
-            double f1 = 0;
-            double f2 = 0;
-            double m1 = 0;
-            double m2 = 0;
         }
     }
 
